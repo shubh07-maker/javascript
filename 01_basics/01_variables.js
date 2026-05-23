1112fa5e49ebbf34a6da3e06ca9a66eadb07a339
shubh07-maker <shubhgoyal2007@gmail.com>
2026-05-23 11:16:33 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId  = 144553
+let accountEmail = "shubhgoyal@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId =2
+accountEmail = "hc@gmail.com"
+accountPassword = "345"
+accountCity = "Agra"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+ 
