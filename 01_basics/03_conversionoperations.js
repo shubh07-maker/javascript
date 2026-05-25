91bdbc4c876ef028a81fabfb590ac9d5940f649c
shubh07-maker <shubhgoyal2007@gmail.com>
2026-05-25 14:09:16 +0000

    Conversion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+// console.log(typeof score)
+let valueInnumber  = Number(score)
+// console.log(typeof valueInnumber)
+// console.log(valueInnumber)
+//"33" = 33
+//"33abc" = NaN
+//true  = 1;false  = 0
+let isLogggedIn  = ""
+let booleanisLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanisLoggedIn)
+//1->true
+//0->false
+//""->false
+//"shubh"->true
+let somenumber = 33
+let typestring  = String(somenumber)
+console.log(typestring)
+console.log(typeof typestring)
+
+
