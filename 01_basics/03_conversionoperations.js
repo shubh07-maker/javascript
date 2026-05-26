f30c7cf4dcb7c5796cd6384389481aa48bb7bcab
shubh07-maker <shubhgoyal2007@gmail.com>
2026-05-26 16:25:02 +0000

    new Change

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -28,6 +28,16 @@ let negvalue = -value
 // console.log("1"+2+3);
 // console.log(+true);
 // console.log(+"");
+let x = 3;
+const y = x++;
+console.log(x);
+console.log(y);
+let a = 3;
+const b = ++a;
+console.log(a);
+console.log(b);
+
+
 
 
 
