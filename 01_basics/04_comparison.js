94ff2dd44c4026446c54d21cf3e51333a406e9ef
shubh07-maker <shubhgoyal2007@gmail.com>
2026-05-27 16:26:14 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(3>2);
+// console.log(3>=2);
+// console.log(2<1);
+
+// console.log("3">2);
+// console.log(null>0);//in java script they works differently;
+// console.log(null==0);
+// console.log(null>=0);
+
+ 
+
+
